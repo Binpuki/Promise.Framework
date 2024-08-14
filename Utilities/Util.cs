e362b7460d9f8d9379043bc979608a248ce5b540
duo <98416575+DuoNineXcore@users.noreply.github.com>
2024-08-14 16:08:05 -0400

    Static Shortcuts for Conductor

--- Utilities/Util.cs
+++ Utilities/Util.cs
@@ -1,41 +1,40 @@
 using System.Linq;
 using Godot;
 
-namespace Promise.Framework.Utilities
+namespace Promise.Framework.Utilities;
+
+/// <summary>
+/// General utility class for the engine.
+/// </summary>
+public static class Util
 {
     /// <summary>
-    /// General utility class for the engine.
+    /// Gets the first child that is of type specified.
     /// </summary>
-    public static class Util
+    /// <typeparam name="T"></typeparam>
+    /// <param name="node"></param>
+    /// <param name="includeInternal"></param>
+    /// <returns></returns>
+    public static T GetChildOfType<T>(this Node node, bool includeInternal = false) where T : class
     {
-        /// <summary>
-        /// Gets the first child that is of type specified.
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="node"></param>
-        /// <param name="includeInternal"></param>
-        /// <returns></returns>
-        public static T GetChildOfType<T>(this Node node, bool includeInternal = false) where T : class
-        {
-            return node.GetChildren(includeInternal).FirstOrDefault(x => x is T) as T;
-        }
+        return node.GetChildren(includeInternal).FirstOrDefault(x => x is T) as T;
+    }
 
-        /// <summary>
-        /// Gets all children that is of type specified.
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="node"></param>
-        /// <param name="includeInternal"></param>
-        /// <returns></returns>
-        public static T[] GetChildrenOfType<T>(this Node node, bool includeInternal = false) where T : class
-        {
-            Node[] nodesFound = node.GetChildren(includeInternal).Where(x => x is T).ToArray();
-            T[] childrenFound = new T[nodesFound.Length];
+    /// <summary>
+    /// Gets all children that is of type specified.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="node"></param>
+    /// <param name="includeInternal"></param>
+    /// <returns></returns>
+    public static T[] GetChildrenOfType<T>(this Node node, bool includeInternal = false) where T : class
+    {
+        Node[] nodesFound = node.GetChildren(includeInternal).Where(x => x is T).ToArray();
+        T[] childrenFound = new T[nodesFound.Length];
 
-            for (int i = 0; i < nodesFound.Length; i++)
-                childrenFound[i] = nodesFound[i] as T;
+        for (int i = 0; i < nodesFound.Length; i++)
+            childrenFound[i] = nodesFound[i] as T;
 
-            return childrenFound;
-        }
+        return childrenFound;
     }
 }
